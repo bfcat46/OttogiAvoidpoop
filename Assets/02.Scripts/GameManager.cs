14969e0b00b1cf98c1b8ba2ec6ae7600f800617e
nakwony <nvvyun@gmail.com>
2024-05-16 21:50:04 +0900

    Feat: UI 및 인터랙션 등 추가
    +환경 설정 - 뒤로가기 버튼 UI
    +뒤로가기 버튼 전용 스크립트

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -4,28 +4,43 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private static GameManager s_instance;
+    public static GameManager Instance => s_instance == null ? null : s_instance;
+
+    public int Score;
+    public int BestScore;
 
     private void Awake()
     {
-        if (s_instance != null) return;
-        s_instance = this;
-        DontDestroyOnLoad(gameObject);
+        if (s_instance == null)
+        {
+            s_instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
-    
-    public static GameManager Instance => s_instance == null ? null : s_instance;
+
 
     public void GameStart()
     {
+        Score = 0;
         SceneManager.LoadScene("MainScene");
     }
-    
-    public void OpenSettings()
-    {
-        SceneManager.LoadScene("SettingScene");
-    }
-    
+
     public void GameOver()
     {
-                
+        //Todo: MainScene 완성 후 추가 예정 => 모달창 띄우기(점수 및 최고 점수 업데이트)
+    }
+
+    public static void OpenStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void OpenSettingScene()
+    {
+        SceneManager.LoadScene("SettingScene");
     }
 }
