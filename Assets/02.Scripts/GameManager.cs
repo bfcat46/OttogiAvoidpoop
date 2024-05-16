6da7b7c74542a288b24b85576c593cd206bf6658
nakwony <nvvyun@gmail.com>
2024-05-16 20:44:21 +0900

    Feat: 시작 화면, 설정 화면, 화면 간 이동 추가

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    private static GameManager s_instance;
 
-    void Update()
+    private void Awake()
     {
-        
+        if (s_instance != null) return;
+        s_instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    public static GameManager Instance => s_instance == null ? null : s_instance;
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("SettingScene");
+    }
+    
+    public void GameOver()
+    {
+                
     }
 }
