f503057e39e86fe76b772b9f81d30317ef9531da
nakwony <nvvyun@gmail.com>
2024-05-17 13:35:40 +0900

    Feat: 버튼 클릭 리스너 스크립트 추가

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -28,21 +28,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        if (SceneManager.GetActiveScene().name != "StartScene") return;
-        
-        if (s_startButton == null)
-        {
-            s_startButton = GameObject.FindWithTag("StartButton").GetComponent<Button>();
-            s_startButton.onClick.AddListener(GameStart);
-        }
-
-        if (s_settingButton != null) return;
-        s_settingButton = GameObject.FindWithTag("SettingButton").GetComponent<Button>();
-        s_settingButton.onClick.AddListener(OpenSettingScene);
-    }
-
     public void GameStart()
     {
         Score = 0;
@@ -58,15 +43,5 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         //Todo: MainScene 완성 후 추가 예정 => 모달창 띄우기(점수 및 최고 점수 업데이트)
-    }
-
-    public void OpenStartScene()
-    {
-        SceneManager.LoadScene("StartScene");
-    }
-
-    public void OpenSettingScene()
-    {
-        SceneManager.LoadScene("SettingScene");
     }
 }
